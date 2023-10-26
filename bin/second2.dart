a986196730926975275fb50ec9a832baeec23828
nurdoolotdavletaliev <nurdoolotdavletaliev435@gmail.com>
2023-10-26 21:39:12 +0600

    test

---
+++ bin/second2.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+
+import 'package:second2/second2.dart' as second2;
+
+void main(){
+final String name787 = 'Otkur';
+const String  name2 = 'otkur';
+
+const int summ = 10;
+int a;
+a = 10;
+
+print(summ);
+
+late String g;
+
+g = 'Nurdoolot';
+
+print(g);
+
+List h = [1,2,3,4,5,6 ];
+print (h.length);
+print(h.reversed);//обратный отщеt print()
+print(h.join('?/'));
+String b = 'My name is Nurdoolot';
+
+List c = b.split('?><?.');
+
+h.insert(3,[1,2,3,4,5,6]);
+
+print(h);
+
+List l = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21232 ];
+print(l);
+}
